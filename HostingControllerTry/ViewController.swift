fbf7e1ec1e223268fdfea0af5e8649dfddff046b
Daryl <daryl.9@gmail.com>
2021-04-30 10:20:23 +0800

    Initial Commit

---
+++ HostingControllerTry/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HostingControllerTry
+//
+//  Created by admin on 30/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
